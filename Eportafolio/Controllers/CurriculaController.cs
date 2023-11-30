9412ae1af0d6187c26d1533d37eaa786c6d74927
justi <justi@JUSTINPC>
2023-11-30 12:07:04 -0800

    Sistema Administrativo (CRUD)

---
+++ Eportafolio/Controllers/CurriculaController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Eportafolio.Models;
+
+namespace Eportafolio.Controllers
+{
+    public class CurriculaController : Controller
+    {
+        private jtrejoEntities db = new jtrejoEntities();
+
+        // GET: Curricula
+        public ActionResult Index()
+        {
+            return View(db.Curriculum.ToList());
+        }
+
+        // GET: Curricula/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Curriculum curriculum = db.Curriculum.Find(id);
+            if (curriculum == null)
+            {
+                return HttpNotFound();
+            }
+            return View(curriculum);
+        }
+
+        // GET: Curricula/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Curricula/Create
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que quiere enlazarse. Para obtener 
+        // más detalles, vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,Area,linkedin,website,Phone,Email,Skills,Tools")] Curriculum curriculum)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Curriculum.Add(curriculum);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(curriculum);
+        }
+
+        // GET: Curricula/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Curriculum curriculum = db.Curriculum.Find(id);
+            if (curriculum == null)
+            {
+                return HttpNotFound();
+            }
+            return View(curriculum);
+        }
+
+        // POST: Curricula/Edit/5
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que quiere enlazarse. Para obtener 
+        // más detalles, vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Area,linkedin,website,Phone,Email,Skills,Tools")] Curriculum curriculum)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(curriculum).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(curriculum);
+        }
+
+        // GET: Curricula/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Curriculum curriculum = db.Curriculum.Find(id);
+            if (curriculum == null)
+            {
+                return HttpNotFound();
+            }
+            return View(curriculum);
+        }
+
+        // POST: Curricula/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Curriculum curriculum = db.Curriculum.Find(id);
+            db.Curriculum.Remove(curriculum);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
